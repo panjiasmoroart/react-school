85c5a0ec4844e4d3395aaa7462987e83ff8b3df4
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-24 22:11:38 +0700

    Installing react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
